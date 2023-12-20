d73f78ef52957f6476f491f01dd68a9296cd7dd0
Nikolay1992167 <wulik1081@mail.ru>
2023-12-20 22:52:36 +0300

    feat: add CheckNullFilter to check CarDto for null.

---
+++ src/main/java/by/clevertec/filter/CheckNullFilter.java
@@ -0,0 +1,47 @@
+package by.clevertec.filter;
+
+import by.clevertec.dto.CarDto;
+import com.google.gson.Gson;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebFilter("/cars/*")
+public class CheckNullFilter extends HttpFilter {
+    private final Gson gson = new Gson();
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        if (req.getMethod().equalsIgnoreCase("POST") || req.getMethod().equalsIgnoreCase("PUT")) {
+
+            BufferedReader readerData = req.getReader();
+            StringBuilder result = new StringBuilder();
+            String line;
+
+            while ((line = readerData.readLine()) != null) {
+                result.append(line);
+            }
+
+            CarDto carDto = gson.fromJson(result.toString(), CarDto.class);
+
+            if (carDto == null) {
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "CarDto cannot be null");
+                return;
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
